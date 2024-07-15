9b2d9168a204651fc35588fcec41050def31c474
ToyaUehara <ueha108@outlook.jp>
2024-07-16 05:00:45 +0900

    add [web.phpの変更]

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/posts', [PostController::class, 'index']);   
